f810c709bec5d7e6871ece160260130410a0b1cf
andreihar <95883512+andreihar@users.noreply.github.com>
2024-04-28 11:04:16 -0700

    Added layout for Converter class

--- taibun/index.js
+++ taibun/index.js
@@ -31,7 +31,176 @@ function toSimplified(input) {
 }
 
 
-// const re = require('re');
+class Converter {
+	static suffix_token = '[ЅFFX_ТКŊ]';
+	static tt = '[ТŊ_ТКŊ]';
+	static DEFAULT_DELIMITER = {};
+	static DEFAULT_SANDHI = {};
+	static __suffixes = ['啊', '矣', '喂', '欸', '唅', '嘿', '諾', '乎', '唷', '喔', '嘖', '的'];
+	static __no_sandhi = ['這', '彼', '遮', '遐'];
+	static __location = ['頂', '跤', '外', '內'];
+
+	constructor(system = 'Tailo', dialect = 'south', format = 'mark', delimiter = Converter.DEFAULT_DELIMITER, sandhi = Converter.DEFAULT_SANDHI, punctuation = 'format', convert_non_cjk = false) {
+		this.system = system.toLowerCase();
+		this.dialect = dialect.toLowerCase();
+		this.format = format;
+		this.delimiter = delimiter;
+		this.sandhi = sandhi;
+		this.punctuation = punctuation;
+		this.convert_non_cjk = convert_non_cjk;
+	}
+
+
+	////// Interface functions
+
+	// Convert tokenised text into specified transliteration system
+	get(input) {
+		return "";
+	}
+
+
+	////// Input formatting
+
+	// Helper to convert separate words
+	convertTokenised(word) {
+		return "";
+	}
+
+
+	// Helper switch for converting 漢字 based on defined transliteration system
+	systemConversion(word) {
+		return "";
+	}
+
+
+	// Helper functions to set delimiter according to transliteration system if wasn't explicitly defined by user
+	setDefaultDelimiter() {
+		return "";
+	}
+
+
+	// Helper functions to set sandhi according to transliteration system if wasn't explicitly defined by user
+	setDefaultSandhi() {
+		return "";
+	}
+
+
+	////// Conversion functions
+
+	// Helper to get number tones
+	getNumberTones(input) {
+		return "";
+	}
+
+
+	// Helper to convert between transliteration systems
+	replacementTool(dictionary, input) {
+		return "";
+	}
+
+
+	// Helper to convert word from Tai-lo to number
+	markToNumber(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo diacritic tones to number tones
+	getNumberTone(input) {
+		return "";
+	}
+
+
+	// Helper to break down a word into syllables for conversion
+	preprocessWord(word) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo number tones to diacritic tones
+	getMarkTone(input, placement, tones) {
+		return "";
+	}
+
+
+	// Helper to apply tone sandhi to a word
+	toneSandhi(word, last) {
+		return "";
+	}
+
+
+	// Helpre to define which words should be sandhi'd fully
+	toneSandhiPosition(input) {
+		return "";
+	}
+
+
+	////// Tai-lo to other transliteration systems converting
+
+	// Helper to convert syllable from Tai-lo to Tai-lo
+	// (called only in cases when tone sandhi is applied)
+	tailoToTailo(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo to POJ
+	tailoToPoj(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo to 方音符號 (zhuyin)
+	tailoToZhuyin(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo to TLPA
+	tailoToTlpa(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo to Bbanlam pingyim
+	tailoToBbanlam(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo to Tong-iong ping-im
+	tailoToTi(input) {
+		return "";
+	}
+
+
+	// Helper to convert syllable from Tai-lo to International Phonetic Alphabet
+	tailoToIpa(input) {
+		return "";
+	}
+
+
+	////// Converted output formatting
+
+	// Helper to convert Chinese punctuation to Latin punctuation with appropriate spacing
+	formatPunctuationWestern(input) {
+		return "";
+	}
+
+
+	// Helper to restore original CJK punctuation with appropriate spacing
+	formatPunctuationCJK(input) {
+		return "";
+	}
+
+
+	// Helper to capitalise text in according to punctuation
+	formatText(input) {
+		return "";
+	}
+}
+
+
 class Tokeniser {
 	constructor() { }
 
