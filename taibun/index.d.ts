f72f8b9642a1b884d260c9304a7cd4bd2bcc4fc4
andreihar <95883512+andreihar@users.noreply.github.com>
2024-05-02 15:16:44 -0700

    Added types

---
+++ taibun/index.d.ts
@@ -0,0 +1,23 @@
+interface ConverterOptions {
+    system?: 'Tailo' | 'POJ' | 'Zhuyin' | 'TLPA' | 'Pingyim' | 'Tongiong' | 'IPA';
+    dialect?: 'south' | 'north';
+    format?: 'mark' | 'number' | 'strip';
+    delimiter?: string;
+    sandhi?: 'auto' | 'none' | 'excLast' | 'inclLast';
+    punctuation?: 'format' | 'none';
+    convertNonCjk?: boolean;
+}
+
+export class Converter {
+    constructor(options?: ConverterOptions);
+    get(input: string): string;
+}
+
+export class Tokeniser {
+    constructor();
+    tokenise(input: string): string[];
+}
+
+export function isCjk(input: string): boolean;
+export function toTraditional(input: string): string;
+export function toSimplified(input: string): string;
